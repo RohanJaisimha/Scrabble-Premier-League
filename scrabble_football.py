773481c277848a2003302d17885a9b5bdeb885e9
RohanJaisimha <33845718+RohanJaisimha@users.noreply.github.com>
2019-06-06 19:43:05 -0500

    Uploaded files

---
+++ scrabble_football.py
@@ -0,0 +1,72 @@
+import unidecode
+
+class Player:
+  def __init__(self,name,position,nationality):
+   self.name=name
+   self.position=position
+   self.nationality=nationality
+   self.scrabble_score=self.get_scrabble_score()
+
+  def get_scrabble_score(self):
+    name_cleaned=clean(unidecode.unidecode(self.name.lower()))
+    count=count_letters(name_cleaned)
+    scrabble_scores={'a':1,'b':3,'c':3,'d':2,'e':1,
+		     'f':4,'g':2,'h':4,'i':1,'j':8,
+		     'k':5,'l':1,'m':3,'n':1,'o':1,
+		     'p':3,'q':10,'r':1,'s':1,'t':1,
+		     'u':1,'v':4,'w':4,'x':8,'y':4,'z':10}
+    score=0
+    for i in count.keys():
+       score=score+count[i]*scrabble_scores[i]
+    return score
+
+  def __str__(self):
+    return self.name+", who plays as a "+self.position+" for "+self.nationality+", has a scrabble score of "+self.scrabble_score
+
+  def __lt__(self,other):
+    return self.scrabble_score<other.scrabble_score
+
+  def __gt__(self,other):
+    return self.scrabble_score>other.scrabble_score
+
+  def __le__(self,other):
+    return not self.__gt__(other)
+
+  def __ge__(self,other):
+    return not self.__lt__(other)
+
+def count_letters(text):
+  count={}
+  for i in text:
+    if(i in count.keys()):
+      count[i]+=1
+    else:
+      count[i]=1
+  return count
+
+def clean(text):
+  for i in range(97):
+    text=text.replace(chr(i),"")
+  for i in range(123,256):
+    text=text.replace(chr(i),"")
+  
+  return text
+
+def main():
+  fin=open("Players_cleaned.txt",'r')
+  players=[]
+  for line in fin:
+    line=line.strip().split("\t")
+    players.append(Player(line[0],line[1],line[2]))
+  fin.close()
+
+  players.sort(reverse=True)
+
+  fout=open("Scrabble_Scores.txt",'w')
+  fout.write("Name\tPosition\tNationality\tScrabble Score\n")
+  for i in players:
+    fout.write(i.name+"\t"+i.position+"\t"+i.nationality+"\t"+str(i.scrabble_score)+"\n")
+  fout.close()
+
+if(__name__=="__main__"):
+  main()
